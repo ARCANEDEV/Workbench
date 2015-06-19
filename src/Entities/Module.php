c73f47402b591dc755bd934ffa7587853509c698
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-06-19 21:27:15 +0000

    Adding the module entity

---
+++ src/Entities/Module.php
@@ -0,0 +1,395 @@
+<?php namespace Arcanedev\Workbench\Entities;
+
+use Arcanedev\Support\Json;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+
+/**
+ * Class Module
+ * @package Arcanedev\Workbench\Entities
+ */
+class Module extends ServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The laravel application instance.
+     *
+     * @var Application
+     */
+
+    protected $app;
+
+    /**
+     * The module name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The module path,.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The constructor.
+     *
+     * @param Application $app
+     * @param $name
+     * @param $path
+     */
+    public function __construct(Application $app, $name, $path)
+    {
+        $this->app  = $app;
+        $this->name = $name;
+        $this->path = realpath($path);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get laravel instance.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function getLaravel()
+    {
+        return $this->app;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get name in lower case.
+     *
+     * @return string
+     */
+    public function getLowerName()
+    {
+        return strtolower($this->name);
+    }
+
+    /**
+     * Get name in studly case.
+     *
+     * @return string
+     */
+    public function getStudlyName()
+    {
+        return Str::studly($this->name);
+    }
+
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->get('description');
+    }
+
+    /**
+     * Get alias.
+     *
+     * @return string
+     */
+    public function getAlias()
+    {
+        return $this->get('alias');
+    }
+
+    /**
+     * Get priority.
+     *
+     * @return string
+     */
+    public function getPriority()
+    {
+        return $this->get('priority');
+    }
+
+    /**
+     * Get path.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set path.
+     *
+     * @param  string $path
+     *
+     * @return self
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Set active state for current module.
+     *
+     * @param  bool $active
+     *
+     * @return bool
+     */
+    public function setActive($active)
+    {
+        return $this->json()->set('active', $active)->save();
+    }
+
+    /**
+     * Get extra path.
+     *
+     * @param $path
+     *
+     * @return string
+     */
+    public function getExtraPath($path)
+    {
+        return $this->getPath() . '/' . $path;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        $this->registerTranslation();
+
+        $this->fireEvent('boot');
+    }
+
+    /**
+     * Register module's translation.
+     *
+     * @return void
+     */
+    protected function registerTranslation()
+    {
+        $lowerName  = $this->getLowerName();
+        $langPath   = base_path("resources/lang/{$lowerName}");
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, $lowerName);
+        }
+    }
+
+    /**
+     * Get json contents.
+     *
+     * @return Json
+     */
+    public function json()
+    {
+        return new Json($this->getPath() . '/module.json', $this->app['files']);
+    }
+
+    /**
+     * Handle call to __get method.
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Get a specific data from json file by given the key.
+     *
+     * @param  string     $key
+     * @param  mixed|null $default
+     *
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        return $this->json()->get($key, $default);
+    }
+
+    /**
+     * Register the module.
+     */
+    public function register()
+    {
+        $this->registerAliases();
+        $this->registerProviders();
+        $this->registerFiles();
+        $this->fireEvent('register');
+    }
+
+    /**
+     * Register the module event.
+     *
+     * @param string $event
+     */
+    protected function fireEvent($event)
+    {
+        $eventName = sprintf('modules.%s.' . $event, $this->getLowerName());
+
+        $this->app['events']->fire($eventName, [$this]);
+    }
+
+    /**
+     * Register the aliases from this module.
+     */
+    protected function registerAliases()
+    {
+        $loader = AliasLoader::getInstance();
+
+        foreach ($this->get('aliases', []) as $aliasName => $aliasClass) {
+            $loader->alias($aliasName, $aliasClass);
+        }
+    }
+
+    /**
+     * Register the service providers from this module.
+     */
+    protected function registerProviders()
+    {
+        foreach ($this->get('providers', []) as $provider) {
+            $this->app->register($provider);
+        }
+    }
+
+    /**
+     * Register the files from this module.
+     */
+    protected function registerFiles()
+    {
+        foreach ($this->get('files', []) as $file) {
+            include $this->path . '/' . $file;
+        }
+    }
+
+    /**
+     * Handle call __toString.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getStudlyName();
+    }
+
+    /**
+     * Enable the current module.
+     */
+    public function enable()
+    {
+        $this->app['events']->fire('module.enabling', [$this]);
+        $this->setActive(true);
+        $this->app['events']->fire('module.enabled', [$this]);
+    }
+
+    /**
+     * Disable the current module.
+     */
+    public function disable()
+    {
+        $this->app['events']->fire('module.disabling', [$this]);
+        $this->setActive(false);
+        $this->app['events']->fire('module.disabled', [$this]);
+    }
+
+    /**
+     * Delete the current module.
+     *
+     * @return bool
+     */
+    public function delete()
+    {
+        return $this->json()->getFilesystem()->deleteDirectory($this->getPath(), true);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Determine whether the given status same with the current module status.
+     *
+     * @param  bool $status
+     *
+     * @return bool
+     */
+    public function isStatus($status)
+    {
+        return $this->get('active', false) == $status;
+    }
+
+    /**
+     * Determine whether the current module activated.
+     *
+     * @return bool
+     */
+    public function enabled()
+    {
+        return $this->active();
+    }
+
+    /**
+     * Alternate for "enabled" method.
+     *
+     * @return bool
+     */
+    public function active()
+    {
+        return $this->isStatus(true);
+    }
+
+    /**
+     * Determine whether the current module not activated.
+     *
+     * @return bool
+     */
+    public function notActive()
+    {
+        return ! $this->active();
+    }
+
+    /**
+     * Alias for "notActive" method.
+     *
+     * @return bool
+     */
+    public function disabled()
+    {
+        return ! $this->enabled();
+    }
+}
