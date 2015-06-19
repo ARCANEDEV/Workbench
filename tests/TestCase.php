67fac13415d3023dc65f42c7abebfb3684622c74
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-06-19 21:27:47 +0000

    Adding the main test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,8 +1,59 @@
 <?php namespace Arcanedev\Workbench\Tests;
 
-use PHPUnit_Framework_TestCase;
+use Arcanedev\Workbench\WorkbenchServiceProvider;
 
-abstract class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        parent::setUp();
 
+        // Your code here
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        //
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            WorkbenchServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Get package aliases.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Workbench' => \Arcanedev\Workbench\Facades\Workbench::class
+        ];
+    }
 }
