acac8424cfd7edd3d0376563c71de2de2d575727
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-06-21 23:56:25 +0000

    Setting the workbench config for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,10 @@
 
 use Arcanedev\Workbench\WorkbenchServiceProvider;
 
+/**
+ * Class TestCase
+ * @package Arcanedev\Workbench\Tests
+ */
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     /* ------------------------------------------------------------------------------------------------
@@ -26,7 +30,24 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
-        //
+        $app['config']->set('workbench.stubs.path',      __DIR__ . '/../stubs');
+        $app['config']->set(
+            'workbench.paths.modules',
+            __DIR__ . '/../build/generated/modules'
+        );
+        $app['config']->set(
+            'workbench.paths.assets',
+            __DIR__ . '/../build/generated/public/modules'
+        );
+        $app['config']->set(
+            'workbench.paths.migration',
+            __DIR__ . '/../build/generated/database/migrations'
+        );
+        $app['config']->set(
+            'workbench.scan.paths', [
+                __DIR__ . '/../vendor/*/*'
+            ]
+        );
     }
 
     /**
